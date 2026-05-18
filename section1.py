88f10dc4ca67815c07c5e099cf83117abe4dfd56
Jeremy Eldredge <jeldred1@byu.edu>
2026-05-18 10:57:35 -0600

    jeremy

---
+++ section1.py
@@ -0,0 +1,15 @@
+cart = []
+def scan_item():
+    add_more = "yes"
+    while add_more == "yes":
+        description = input("Item description: ")
+        price = float(input("Item price: "))
+        quantity = int(input("Quantity: "))
+        cart.append({'description' : description, 'price' : price, 'quantity' : quantity})
+        add_more = input("Would you like to scan another item? (yes/no)").lower()
+    return cart
+
+def calculate_subtotal():
+    for item in cart:
+        subtotal += item('price')
+    return subtotal
